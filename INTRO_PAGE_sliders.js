8da2f23e9c4939313ac7b5f6f95dc53c81985c91
Brendan O'Donnell <odonnell.abu@gmail.com>
2019-07-15 15:56:19 -0400

    change sliders to Dat.GUI
    got rid of old sliders and switched to DAT GUI

--- INTRO_PAGE_sliders.js
+++ INTRO_PAGE_sliders.js
@@ -1,13 +1,27 @@
+
+//dat gui stuff
+var sliders;
+
 var driver = 0.0;
-var slider_numSqaures;
 var slider_speed;
 var slider_freq;
 var slider_textSize;
 var slider_amp;
-var shape = 1;
+//var shape = 1;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+ 
+//dat gui stuff
+sliders = new Slider();
+var gui = new dat.GUI();
+gui.add(sliders,'Total_Shapes',1,200);
+gui.add(sliders,'Speed',1,10);
+gui.add(sliders,'Frequency',0,0.05);
+gui.add(sliders,'Amplitude',0,0.5);
+gui.add(sliders, 'Shape', { Circle: 0, Square: 1, Line: 2 } );
+
 
   // text spec
   fill(255, 10);
@@ -15,24 +29,6 @@ function setup() {
   //textSize(0);
   textFont('Jura');
 
-  //sliders
-  slider_numSqaures = createSlider(0, 150, 25);
-  slider_numSqaures.position(width/40, 30);
-  slider_numSqaures.style('width', '180px');
-
-  slider_speed = createSlider(0, 200, 50);
-  slider_speed.position(width/40, 70);
-  slider_speed.style('width', '180px');
-
-  slider_freq = createSlider(0, 500, 250);
-  slider_freq.position(width/40, 110);
-  slider_freq.style('width', '180px');
-
-
-
-  slider_amp = createSlider(0, 500, 250);
-  slider_amp.position(width/40, 150);
-  slider_amp.style('width', '180px');
 }
 
 
@@ -49,59 +45,43 @@ function draw() {
   }
   textSize(slider_textSize);
 
-  var val_numSquares = slider_numSqaures.value();
-  text("Number of shapes", width/40, 30);
-
-  var val_speed = slider_speed.value();
-  text("Accelerate", width/40, 65); 
-
-  var val_freq = slider_freq.value()*0.00001;
-  text("Frequency", width/40, 110);
 
 
-  var val_amp = slider_amp.value()*0.001;
-  text("Amplitude", width/40, 145);
-  
+//  //click to change shape 
+//  //circle
+//  if (  mouseIsPressed &&   dist((width/40)+10, 190, mouseX, mouseY)<10              ) { 
+//    shape =0;
+//  }
+//  if (shape ==0) {
+//    fill(255);
+//  } else {
+//    noFill();
+//  }
+//  ellipse((width/40)+10, 190, 20, 20);
 
-  //click to change shape 
-  //circle
-  if (  mouseIsPressed &&   dist((width/40)+10, 190, mouseX, mouseY)<10              ) { 
-    shape =0;
-  }
-  if (shape ==0) {
-    fill(255);
-  } else {
-    noFill();
-  }
-  ellipse((width/40)+10, 190, 20, 20);
+  ////square
+  //if (  mouseIsPressed &&   dist((width/40)+40, 190, mouseX, mouseY)<10              ) { 
+  //  shape =1;
+  //}
+  //if (shape ==1) {
+  //  fill(255);
+  //} else {
+  //  noFill();
+  //}
+  //rect((width/40)+40, 190, 20, 20);
 
-  //square
-  if (  mouseIsPressed &&   dist((width/40)+40, 190, mouseX, mouseY)<10              ) { 
-    shape =1;
-  }
-  if (shape ==1) {
-    fill(255);
-  } else {
-    noFill();
-  }
-  rect((width/40)+40, 190, 20, 20);
-  
-  // lines
- if (  mouseIsPressed &&   dist((width/40)+75, 190, mouseX, mouseY)<15              ) { 
-    shape =2;
-  }
-   if (shape ==2) {
-    fill(255);
-  } else {
-    noFill();
-  }
-  rect((width/40)+75, 190, 30, 5);
+  //// lines
+  //if (  mouseIsPressed &&   dist((width/40)+75, 190, mouseX, mouseY)<15              ) { 
+  //  shape =2;
+  //}
+  //if (shape ==2) {
+  //  fill(255);
+  //} else {
+  //  noFill();
+  //}
+ // rect((width/40)+75, 190, 30, 5);
 
-  
-  
-  
-  
-  
+
   textSize(width*0.02);
 
   fill(255, 50);
@@ -113,7 +93,7 @@ function draw() {
   translate(width/2, height/2);
 
 
-  if (shape ==1) {
+  if (sliders.Shape ==1) {
     push();
     rotate(QUARTER_PI);
     rect(0, 0, height/2, height/2);
@@ -123,42 +103,37 @@ function draw() {
   }
 
 
-
-
-
-
-
   noFill();
   stroke(255);
   rectMode(CENTER);
-  for (var i =0; i<val_numSquares; i++) {
+  for (var i =0; i<sliders.Total_Shapes; i++) {
     stroke(255, 50);
     push();
-    rotate(i*val_speed);
+    rotate(i*sliders.Speed);
 
 
-    if (shape ==0) {
-      ellipse(cosx(val_freq*i, (height*val_amp), driver+i), siny(val_freq*i, (height*val_amp)+i, driver), height*0.09, height*0.09);
-      ellipse(cosx(val_freq*i, (-height*val_amp/2), driver), siny(val_freq*i, (-height*val_amp/2)+i, driver), height*0.03, height*0.03);
+    if (sliders.Shape ==0) {
+      ellipse(cosx(sliders.Frequency*i, (height*sliders.Amplitude), driver+i), siny(sliders.Frequency*i, (height*sliders.Amplitude)+i, driver), height*0.09, height*0.09);
+      ellipse(cosx(sliders.Frequency*i, (-height*sliders.Amplitude/2), driver), siny(sliders.Frequency*i, (-height*sliders.Amplitude/2)+i, driver), height*0.03, height*0.03);
     }
 
-    if (shape == 1) {
+    if (sliders.Shape == 1) {
 
-      rect(cosx(val_freq*i, (height*val_amp), driver), siny(val_freq*i, (height*val_amp)+i, driver), height*0.09, height*0.09);
-      rect(cosx(val_freq*i, (-height*val_amp/2), driver), siny(val_freq*i, (-height*val_amp/2)+i, driver), height*0.03, height*0.03);
+      rect(cosx(sliders.Frequency*i, (height*sliders.Amplitude), driver), siny(sliders.Frequency*i, (height*sliders.Amplitude)+i, driver), height*0.09, height*0.09);
+      rect(cosx(sliders.Frequency*i, (-height*sliders.Amplitude/2), driver), siny(sliders.Frequency*i, (-height*sliders.Amplitude/2)+i, driver), height*0.03, height*0.03);
     }
 
 
- if (shape == 2) {
+    if (sliders.Shape == 2) {
 
-      line(cosx(val_freq*i, (height*val_amp), driver), siny(val_freq*i, (height*val_amp)+i, driver), height*0.09, height*0.09);
-      line(cosx(val_freq*i, (-height*val_amp/2), driver), siny(val_freq*i, (-height*val_amp/2)+i, driver), height*0.03, height*0.03);
+      line(cosx(sliders.Frequency*i, (height*sliders.Amplitude), driver), siny(sliders.Frequency*i, (height*sliders.Amplitude)+i, driver), height*0.09, height*0.09);
+      line(cosx(sliders.Frequency*i, (-height*sliders.Amplitude/2), driver), siny(sliders.Frequency*i, (-height*sliders.Amplitude/2)+i, driver), height*0.03, height*0.03);
     }
 
 
     pop();
   }
-  if (shape ==1) {
+  if (sliders.Shape ==1) {
     push();
     rotate(driver*0.05);
     rect(0, 0, height*0.1, height*0.1);
@@ -172,8 +147,7 @@ function draw() {
 
 
 
-  driver +=(val_speed*0.008);
-  print(shape);
+  driver +=(sliders.Speed*0.008);
 }
 
 
@@ -186,4 +160,13 @@ function cosx( freq, amp, driver) {
 function siny( freq, amp, driver) {
 
   return (sin(driver*freq)*amp);
+}
+
+function Slider(){
+  this.Total_Shapes = 10;
+  this.Speed =5;
+  this.Frequency = 0.01;
+  this.Amplitude = 0.25;
+  this.Shape =1;
+
 }
